8a407e6f6c4bd6eaa1ba57ddf90be3768eb9713a
Abdurahim Ganiev <aganiev@nes.ru>
2021-11-14 15:52:51 +0300

    updated hw4

--- avito_hw4.py
+++ avito_hw4.py
@@ -1,4 +1,5 @@
-import json
+﻿import json
+import keyword
 
 lesson_str = """{
                 "title": "python",
@@ -28,36 +29,43 @@ dog_ad = """{
 
 
 class ColorizeMixin:
+    """Класс который перекрашивает цвет текста объявления"""
     def __repr__(self):
-        return f'\033[0;{self.repr_color_code};{self.repr_color_code}m{self.title} | {self.price} ₽\n'
+        text = super().__repr__()
+        return f'\033[0;{self.repr_color_code};{self.repr_color_code}m{text}\n'
 
+    
+class BaseAdvert:
+    """Класс, который преобразовывает json в dict """
+    def __repr__(self):
+        return f'{self.title} | {self.price}₽'
+    
 
+class JsonParser:
+    def __init__(self, adv):
+        adv_info = json.loads(adv)
+        
+        for key, value in adv_info.items():
+            if isinstance(value, dict):
+                value = JsonParser(json.dumps(value))
+            
+            if keyword.iskeyword(key):
+                key = key + '_'
+            
+            self.__dict__[key] = value
 
-class Advert(ColorizeMixin):
+            
+class Advert(ColorizeMixin, JsonParser, BaseAdvert):
     
     """Класс объяслений, который показывает название, цену тип и другие параметры объявления"""
     
     repr_color_code = 32
-    
-    def __init__(self, adv: json):
-        adv_info = json.loads(adv)
-        self.__dict__.update(adv_info)
-    
-    @property        
-    def class_(self):
-        self.__dict__['class'] = self.__dict__.get('class', None)
-        return self.__dict__['class']
-         
+            
     @property
     def price(self):
         self.__dict__['price'] = self.__dict__.get('price', 0)
         if self.__dict__['price'] < 0:
-            return ValueError('price must be >= 0')
+            raise ValueError('price must be >= 0')
         else:
-            return self.__dict__['price']
-        
-
-if __name__ == '__main__':
-    lesson = Advert(lesson_str)
-    iphone = Advert(iphone_ad)
-    dog_ad = Advert(dog_ad)
+            return self.__dict__['price']         
+            
