5e5d0bdc9fccbe712eb51224eddf2d8fcd33e181
Abdurahim Ganiev <aganiev@nes.ru>
2021-12-12 20:35:48 +0300

    added hw5 AAA

---
+++ avito_hw5.py
@@ -0,0 +1,55 @@
+import sys
+from datetime import datetime
+from typing import Callable
+
+original_write = sys.stdout.write
+
+
+def my_write(string_text: str):
+    """Функция добавляет время вывода на экран"""
+    original_write(f'[{str(datetime.now())[0:-7]}]: {string_text}' + '\n')
+
+
+sys.stdout.write = my_write
+my_write('1, 2, 3')
+sys.stdout.write = original_write
+
+
+def timed_output(function: Callable):
+    '''Декоратор, который показывает время вывода на экран'''
+    change_write = original_write
+
+    def wrapper(string_text):
+        change_write(f'[{str(datetime.now())[0:-7]}]: ')
+        function(string_text)
+    return wrapper
+
+
+@timed_output
+def print_greeting(name):
+    print(f'Hello, {name}!')
+
+
+print_greeting("Nikita")
+
+
+def redirect_output(filepath):
+    """Декоратор, которые записывает все выводы функции print в файл. Файл будет храниться в filepath """
+    def wrapper(func):
+        def inner_wrapper():
+            sys.stdout = open(filepath, 'w')
+            result = func()
+            return result
+        return inner_wrapper
+    return wrapper
+
+
+@redirect_output(r'C:\Users\Abdurakhim\Desktop\function_output.txt')
+def calculate():
+    for power in range(1, 5):
+        for num in range(1, 20):
+            print(num ** power, end=' ')
+        print()
+
+
+calculate()
